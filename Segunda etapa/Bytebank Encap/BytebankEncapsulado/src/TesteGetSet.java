0d9f5d433840d51d567b8fd7d809cee4bcad9356
Ranieeery <raniery2003@hotmail.com>
2023-02-16 18:31:26 -0300

    [add] more java code snippets

---
+++ Segunda etapa/Bytebank Encap/BytebankEncapsulado/src/TesteGetSet.java
@@ -0,0 +1,8 @@
+public class TesteGetSet {
+  public static void main(String[] args) {
+    Conta conta = new Conta();
+
+    conta.setNumero(1337);
+    System.out.println(conta.getNumero());
+  }
+}

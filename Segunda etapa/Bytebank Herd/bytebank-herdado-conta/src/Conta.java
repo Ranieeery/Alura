7a01d9f9451e9156af482add55ac839b724ab98c
Ranieeery <raniery2003@hotmail.com>
2023-03-12 10:59:54 -0300

    [add] inheritance, constructor, super, override, abstract class and polymorphism

---
+++ Segunda etapa/Bytebank Herd/bytebank-herdado-conta/src/Conta.java
@@ -0,0 +1,75 @@
+class Conta {
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+    private static int total = 0;
+
+    public Conta(int agencia, int numero) {
+        Conta.total++;
+        this.agencia = agencia;
+        this.numero = numero;
+        System.out.println("Estou criando uma conta " + this.numero);
+        System.out.println("O total de contas existentes é " + Conta.total);
+    }
+
+    public Conta(int agencia, int numero, String tipoConta) {
+        Conta.total++;
+        this.agencia = agencia;
+        this.numero = numero;
+        System.out.println("Estou criando uma conta " + this.numero + " do tipo: " + tipoConta);
+        System.out.println("O total de contas existentes é " + Conta.total);
+    }
+    
+    public void deposito(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean saque(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else
+            return false;
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saque(valor)) {
+            destino.deposito(valor);
+            return true;
+        } else
+            return false;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public void setTitular(Cliente titular) {
+      this.titular = titular;
+    }
+
+    public static int getTotal() {
+      return Conta.total;
+    }
+}
---
+++ Segunda etapa/Bytebank Herd/bytebank-herdado-conta/src/ContaCorrente.java
@@ -0,0 +1,11 @@
+public class ContaCorrente extends Conta{
+  public ContaCorrente(int agencia, int numero) {
+    super(agencia, numero, "Conta Corrente");
+  }
+
+  @Override
+  public boolean saque(double valor) {
+    double valorSaque = valor + 0.5;
+    return super.saque(valorSaque);
+  }
+}

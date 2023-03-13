bfceaf38ca99a977251d95febcccc67c165bc8a6
Ranieeery <raniery2003@hotmail.com>
2023-03-13 01:54:30 -0300

    [add] classe intermediaria para herdar autenticacao

--- Segunda etapa/Bytebank Herd/bytebank-herdado/src/Gerente.java
+++ Segunda etapa/Bytebank Herd/bytebank-herdado/src/Gerente.java
@@ -1,5 +1,4 @@
-public class Gerente extends Funcionario{
-  private int senha;
+public class Gerente extends FuncionarioAutenticavel{
 
   public Gerente(String nome, String cpf, double salario, int senha) {
     super(nome, cpf, salario);
@@ -14,15 +13,5 @@ public class Gerente extends Funcionario{
   // public double getBonificacao() {
   //   return this.getSalario();
   // }  
-
-  public boolean autenticacao(int senha) {
-    if (this.senha == senha) {
-      System.out.println("Acesso permitido!");
-      return true;
-    } else {
-      System.out.println("Acesso negado!");
-      return false;
-    }
-  }
 }
     

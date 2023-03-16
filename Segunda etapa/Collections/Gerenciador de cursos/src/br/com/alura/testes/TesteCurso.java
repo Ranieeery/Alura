843989b070e9737f68dd0412f7a13e7233a7647e
Ranieeery <raniery2003@hotmail.com>
2023-03-16 02:29:52 -0300

    [add] Estudo de Collections e Map

--- Segunda etapa/Collections/Gerenciador de cursos/src/br/com/alura/testes/TesteCurso.java
+++ Segunda etapa/Collections/Gerenciador de cursos/src/br/com/alura/testes/TesteCurso.java
@@ -1,5 +1,9 @@
 package br.com.alura.testes;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import br.com.alura.Aula;
 import br.com.alura.Curso;
 
@@ -9,6 +13,16 @@ public class TesteCurso {
     curso.add(new Aula("Trabalhando com ArrayList", 21));
     curso.add(new Aula("Criando uma aula", 20));
     curso.add(new Aula("Modelando com coleções", 24));
-    System.out.println(curso.getAulas());
+
+    List<Aula> aulasImutaveis = curso.getAulas();
+    System.out.println(aulasImutaveis);
+
+    List<Aula> aulas = new ArrayList<>(aulasImutaveis);
+    
+    Collections.sort(aulas);
+    System.out.println(aulas);
+    System.out.println(curso.getTempoTotal());
+
+    System.out.println(curso);
   }
 }

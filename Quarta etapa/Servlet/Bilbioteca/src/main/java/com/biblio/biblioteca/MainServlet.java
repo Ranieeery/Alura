32bb222d018c77782dfefe8267e568a43e6a8dc5
Ranieeery <raniery2003@hotmail.com>
2023-04-02 15:47:41 -0300

    [new] Java Servlet - HelloServlet

--- Quarta etapa/Servlet/Bilbioteca/src/main/java/com/biblio/biblioteca/MainServlet.java
+++ Quarta etapa/Servlet/Bilbioteca/src/main/java/com/biblio/biblioteca/MainServlet.java
@@ -1,2 +1,34 @@
-package com.biblio.biblioteca;public class MainServlet {
+package com.biblio.biblioteca;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "mainServlet", value = "/main-servlet")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter writer = response.getWriter();
+        writer.print("Meus livros");
+
+        writer.print("<ul>");
+
+        writer.print("<li>");
+        writer.print("Livro de Java Web");
+        writer.print("</li>");
+
+        writer.print("<li>");
+        writer.print("Livro de Android");
+        writer.print("</li>");
+
+        writer.print("</ul>");
+    }
 }

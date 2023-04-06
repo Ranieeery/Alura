fe4ebde1bc54ab77c7bea4de2b3cbe96de63c677
Ranieeery <raniery2003@hotmail.com>
2023-04-06 16:59:25 -0300

    :sparkles: feat - access TokenService

--- Quarta etapa/Spring Boot 3/src/main/java/com/med/api/infra/security/TokenService.java
+++ Quarta etapa/Spring Boot 3/src/main/java/com/med/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.med.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Não foi possível gerar o token", exception);
+        }
+    }
+
+    public String getSubject(String token) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API médico")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Não foi possível validar o token", exception);
         }
     }
 

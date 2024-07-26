3c444203ee7c93383547410fde59b674c1831157
Ranieeery <raniery2003@hotmail.com>
2024-07-26 12:51:53 -0300

    feat: add methods(deposica, saca, transfere)

--- src/Conta.java
+++ src/Conta.java
@@ -3,4 +3,20 @@ class Conta {
     int agencia;
     int numero;
     String titular;
+
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    public void saca(double valor){
+        this.saldo -= valor;
+    }
+
+    public boolean transfere(double valor, Conta contatrans) throws Exception{
+        if (this.saldo <= valor) throw new Exception("Saldo insuficiente para a transferência");
+            this.saca(valor);
+            contatrans.deposita(valor);
+            return true;
+        }
+
 }
---
+++ src/TesteMetodo.java
@@ -0,0 +1,16 @@
+public class TesteMetodo {
+    public static void main(String[] args) {
+        Conta contaPedro = new Conta();
+
+        contaPedro.deposita(500);
+        System.out.printf("O valor na conta é R$%.2f\n", contaPedro.saldo);
+
+        contaPedro.saca(150);
+        System.out.printf("O valor na conta é R$%.2f\n", contaPedro.saldo);
+
+        Conta contaJoao = new Conta();
+
+        contaJoao.deposita(200);
+        contaJoao.transfere(120, contaJoao);
+    }
+}

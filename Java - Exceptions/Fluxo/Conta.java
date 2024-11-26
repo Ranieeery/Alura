3d498058ac62c0d5500f0e2df02cf02fad54849d
Ranieeery <raniery2003@hotmail.com>
2024-11-26 10:52:58 -0300

    feat: Add Conexao and TesteConexao classes, enhance Conta class with deposit and withdrawal methods

--- Java - Exceptions/Fluxo/Conta.java
+++ Java - Exceptions/Fluxo/Conta.java
@@ -1,5 +1,23 @@
 public class Conta {
-    void deposita() throws MyException {
+    private double saldo;
 
+    public void deposita() throws MyException {
+        throw new MyException("Método não implementado");
+    }
+
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    public void saca(double valor) throws MyException {
+        if (this.saldo < valor) {
+            throw new MyException("Saldo insuficiente");
+        } else {
+            this.saldo -= valor;
+        }
+    }
+
+    public double getSaldo() {
+        return this.saldo;
     }
 }

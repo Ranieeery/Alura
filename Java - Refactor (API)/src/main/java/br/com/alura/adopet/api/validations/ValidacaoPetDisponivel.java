92579b828b1a0c3c165e5bfab5c9c03a15a2d0dc
Ranieeery <raniery2003@hotmail.com>
2024-12-29 15:21:12 -0300

    refactor: implement validation logic for adoption requests using strategy pattern

---
+++ Java - Refactor (API)/src/main/java/br/com/alura/adopet/api/validations/ValidacaoPetDisponivel.java
@@ -0,0 +1,25 @@
+package br.com.alura.adopet.api.validations;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDTO;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidacaoPetDisponivel implements ValidacaoSolicitacaoAdocao {
+
+    private final PetRepository petRepository;
+
+    public ValidacaoPetDisponivel(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    public void validar(SolicitacaoAdocaoDTO dto) {
+        Pet pet = petRepository.getReferenceById(dto.idPet());
+
+        if (pet.getAdotado()) {
+            throw new ValidacaoException("Pet já foi adotado!");
+        }
+    }
+}

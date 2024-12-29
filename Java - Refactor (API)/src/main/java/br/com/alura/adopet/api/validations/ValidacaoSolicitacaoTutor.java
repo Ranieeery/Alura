4ac623f0e2a22076a9ad9296db8581f5f139578a
Ranieeery <raniery2003@hotmail.com>
2024-12-29 20:11:43 -0300

    refactor: implement Tutor registration using DTO and validation strategy

---
+++ Java - Refactor (API)/src/main/java/br/com/alura/adopet/api/validations/ValidacaoSolicitacaoTutor.java
@@ -0,0 +1,7 @@
+package br.com.alura.adopet.api.validations;
+
+import br.com.alura.adopet.api.dto.SolicitacaoCadastrarTutorDTO;
+
+public interface ValidacaoSolicitacaoTutor {
+    public void validar(SolicitacaoCadastrarTutorDTO dto);
+}
---
+++ Java - Refactor (API)/src/main/java/br/com/alura/adopet/api/validations/ValidacaoTutorExistente.java
@@ -0,0 +1,23 @@
+package br.com.alura.adopet.api.validations;
+
+import br.com.alura.adopet.api.dto.SolicitacaoCadastrarTutorDTO;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.repository.TutorRepository;
+
+public class ValidacaoTutorExistente implements ValidacaoSolicitacaoTutor {
+
+    private final TutorRepository repository;
+
+    public ValidacaoTutorExistente(TutorRepository repository) {
+        this.repository = repository;
+    }
+
+    public void validar(SolicitacaoCadastrarTutorDTO dto) {
+        boolean telefoneJaCadastrado = repository.existsByTelefone(dto.telefone());
+        boolean emailJaCadastrado = repository.existsByEmail(dto.email());
+
+        if (telefoneJaCadastrado || emailJaCadastrado) {
+            throw new ValidacaoException("Dados já cadastrados para outro tutor!");
+        }
+    }
+}

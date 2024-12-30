cf1897664fede39afde8f2b29cb10846517d7814
Ranieeery <raniery2003@hotmail.com>
2024-12-30 01:07:03 -0300

    refactor: add AbrigoDTO for shelter data representation and rename SolicitacaoListarPetsDTO to PetDto

--- Java - Refactor (API)/src/main/java/br/com/alura/adopet/api/dto/PetDto.java
+++ Java - Refactor (API)/src/main/java/br/com/alura/adopet/api/dto/PetDto.java
@@ -1,26 +1,27 @@
 package br.com.alura.adopet.api.dto;
 
+import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.TipoPet;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record SolicitacaoListarPetsDTO(
 
-    @NotBlank
+    Long id,
+
     TipoPet tipo,
 
-    @NotBlank
     String nome,
 
-    @NotBlank
     String raca,
 
-    @NotNull
     Integer idade,
 
-    @NotBlank
     String cor,
 
-    @NotNull
     Float peso) {
+
+    public SolicitacaoListarPetsDTO(Pet pet) {
+        this(pet.getId(), pet.getTipo(), pet.getNome(), pet.getRaca(), pet.getIdade(), pet.getCor(), pet.getPeso());
+    }
 }

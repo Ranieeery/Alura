a7ae925421d07a07e098cf4d7442345776b645a9
Ranieeery <raniery2003@hotmail.com>
2025-01-01 21:23:27 -0300

    refactor: remove deprecated OpenAI client and update integration test to use SimpleOpenAI

--- Java - IA/openai-java/src/main/java/dev/raniery/ecommerce/IntegrationTest.java
+++ Java - IA/openai-java/src/main/java/dev/raniery/ecommerce/IntegrationTest.java
@@ -1,11 +1,10 @@
 package dev.raniery.ecommerce;
 
-import com.openai.client.OpenAIClient;
-import com.openai.client.okhttp.OpenAIOkHttpClient;
-import com.openai.models.*;
 import io.github.cdimascio.dotenv.Dotenv;
-
-import java.util.List;
+import io.github.sashirestela.openai.SimpleOpenAI;
+import io.github.sashirestela.openai.domain.chat.ChatMessage.SystemMessage;
+import io.github.sashirestela.openai.domain.chat.ChatMessage.UserMessage;
+import io.github.sashirestela.openai.domain.chat.ChatRequest;
 
 public class IntegrationTest {
 
@@ -15,25 +14,20 @@ public class IntegrationTest {
         String systemMessage = "You are a data scientist working for an e-commerce company. You have been tasked with generating 5 product recommendations for a user based on their purchase history. The user has purchased the following items: books, a laptop, and a pair of shoes.";
 
         assert OPENAI_API_KEY != null;
-        OpenAIClient client = OpenAIOkHttpClient.builder()
-            .apiKey(OPENAI_API_KEY)
-            .build();
+        var openAI = SimpleOpenAI.builder()
+                .apiKey(OPENAI_API_KEY)
+                .build();
 
-        ChatCompletionCreateParams params = ChatCompletionCreateParams.builder()
-            .messages(List.of(ChatCompletionMessageParam.ofChatCompletionUserMessageParam(
-                ChatCompletionSystemMessageParam.builder()
-                    .additionalProperties(userMessage),
-                ChatCompletionUserMessageParam.builder()
-                    .role(ChatCompletionUserMessageParam.Role.USER)
-                    .content(ChatCompletionUserMessageParam.Content.ofTextContent(systemMessage))
-                    .build())))
-            .model(ChatModel.GPT_4O_MINI)
-            .build();
-        ChatCompletion chatCompletion = client.chat().completions().create(params);
+        var chatRequest = ChatRequest.builder()
+                .model("gpt-4o-mini")
+                .message(SystemMessage.of(userMessage))
+                .message(UserMessage.of(systemMessage))
+                .temperature(0.0)
+                .maxCompletionTokens(300)
+                .build();
+        var futureChat = openAI.chatCompletions().create(chatRequest);
+        var chatResponse = futureChat.join();
+        System.out.println(chatResponse.firstContent());
 
-        List<ChatCompletion.Choice> choices = chatCompletion.choices();
-        for (ChatCompletion.Choice choice : choices) {
-            System.out.println("Choice content: " + choice.message().content().get());
-        }
     }
 }

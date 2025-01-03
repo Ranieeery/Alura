911e6bf39dcacf6aa939a95e06759cd317d723cf
Ranieeery <raniery2003@hotmail.com>
2025-01-02 22:00:21 -0300

    test: add JUnit 5 dependency and create initial CategoriesTest class

---
+++ Java - IA/openai-java/src/test/java/dev/raniery/ecommerce/details/CategoriesTest.java
@@ -0,0 +1,13 @@
+package dev.raniery.ecommerce.details;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoriesTest {
+
+    @Test
+    void helloWorld() {
+        assertEquals(1, 1);
+    }
+}
